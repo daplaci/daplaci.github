0e84f2b5151d58f906da95d91404f54cf5c9d242
daplaci <davide.placido@cpr.ku.dk>
2024-06-10 09:30:39 +0200

    update cv test-3

--- render_pdf.sh
+++ render_pdf.sh
@@ -6,11 +6,8 @@ PDF_FILE="assets/pdf/davide_placido_cv.pdf"
 
 # Command to render JSON to PDF
 # Replace this with the actual command you use to render the PDF
-json_to_html="resume export resume.html --theme ~/Documents/GitHub/jsonresume-theme-macchiato --resume $JSON_FILE"
-html_to_pdf="puppeteer --margin-top 0 --margin-right 0 --margin-bottom 0 --margin-left 0 --format A4 print resume.html $PDF_FILE"
-
-$json_to_html
-$html_to_pdf
+resume export resume.html --theme ~/Documents/GitHub/jsonresume-theme-macchiato --resume $JSON_FILE
+puppeteer --margin-top 0 --margin-right 0 --margin-bottom 0 --margin-left 0 --format A4 print resume.html $PDF_FILE
 rm resume.html
 
 # Add the rendered PDF to the commit
